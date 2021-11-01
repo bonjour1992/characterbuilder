8731ad8eda9e1488dfaa2da1c532edcaeb954c95
bonjour1992 <bonjour1992@laposte.net>
2021-11-01 14:40:52 +0100

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -3,4 +3,6 @@ import  origine from './origine.mjs'
 export function load()
 {
 	console.log(origine)
+	var opt= document.createElement("option").appendChild(document.createTextNode(origine.index[0]))
+	documentGetElementById("l_origine").appendChild()
 }
